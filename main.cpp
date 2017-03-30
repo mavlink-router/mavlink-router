0037fce445a05b4ae6898d8880c567ca900ba016
Ederson de Souza <ederson.desouza@intel.com>
2017-03-30 14:46:48 -0700

    main: Fix wrong argument expansion
    It was converting `--conf-file` to `-i`, instead of `-c`

--- main.cpp
+++ main.cpp
@@ -313,7 +313,7 @@ static int parse_argv(int argc, char *argv[])
     static const struct option options[] = {
         { "baudrate",               required_argument,  NULL,   'b' },
         { "endpoints",              required_argument,  NULL,   'e' },
-        { "conf-file",              required_argument,  NULL,   'i' },
+        { "conf-file",              required_argument,  NULL,   'c' },
         { "conf-dir" ,              required_argument,  NULL,   'd' },
         { "report_msg_statistics",  no_argument,        NULL,   'r' },
         { "tcp-port",               required_argument,  NULL,   't' },
