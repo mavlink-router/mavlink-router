840504220721c2d886ba664b859e98f202cf505c
Otavio Pontes <otavio.pontes@intel.com>
2017-04-07 15:43:12 -0700

    conf: Change ConfFile API
    Change ConfFile API according to suggestions on camera-streaming-daemon
    git repository. As the ConfFile class will be shared between both
    projects, it is necessary to attend necessities of both of them.
    
    ConfFile now loads information about all configuration files read and
    keep pointers (and offsets) to each configuration, avoiding string
    duplications. Each new file added will overload configurations from
    previous files, so users don't need to check that.

--- conf_file.h
+++ conf_file.h
@@ -17,24 +17,139 @@
  */
 #pragma once
 
+/**
+ * Load and parse multiple conf files, offering methods to extract the configuration options to user
+ * structs.
+ */
 class ConfFile {
 public:
-    ConfFile(const char *filename)
-        : _filename{filename} {};
-
+    ConfFile()
+        : _files(nullptr)
+        , _sections(nullptr){};
     ~ConfFile();
 
-    int parse_file();
-    const char *next_from_section(const char *section_name, const char *key);
-    const char *first_section();
-    const char *next_section();
+    struct section_iter {
+        const char *name;
+        size_t name_len;
+        void *ptr;
+    };
+
+    /**
+     * Table with information on how to parse options from conf file.
+     *
+     * @param key The key to the option field
+     * @param required If the field is required, extract_options() will fail if it is not found
+     * @param parser_func Function to parse the value of the desired option, where val is the
+     * pointer to value as string, val_len is size of the value, storage is a pointer to the area to
+     * store the parsed value and storage_len is the length of the storage area.
+     * @param storage.offset The offset from the beggining of the struct used in extract_options()
+     * to store the values.
+     * @param storage.len The length in bytes of the storage poited by @a storage_offset.
+     */
+    struct OptionsTable {
+        const char *key;
+        bool required;
+        int (*parser_func)(const char *val, size_t val_len, void *storage, size_t storage_len);
+        struct {
+            off_t offset;
+            size_t len;
+        } storage;
+    };
+
+    /**
+     * Load and parse a configuration file.
+     *
+     * Open the file using mmap and parse it, keeping an internal structure with pointers to conf
+     * sections and options, in order to do a quick extraction using extract_options().
+     * When an option in a section is already present before parse is called, the option is
+     * overrided by the option from the new file. Other options from sections are not changed.
+     * Files are kept open until release_all() is called or the ConfFile object is destructed.
+     *
+     * @param filename The conf filename
+     * @return errno on IO or parsing errors or @c 0 if successful
+     */
+    int parse(const char *filename);
+
+    /**
+     * Release all opened files and internal structures from this ConfFile.
+     */
+    void release_all();
+
+    /**
+     * Extract options set in @a table from section @a section_name
+     *
+     * @param section_name Name of the section to extract options.
+     * @param table Array of TableOption with information on which fields are going to be
+     * extracted and how to extract them.
+     * @param table_len The number of elements in @a table.
+     * @param data A pointer to the struct that will be used to hold the extracted data.
+     */
+    int extract_options(const char *section_name, const OptionsTable table[], size_t table_len,
+                        void *data);
+
+    /**
+     * Extract options set in @a table from section @a iter
+     *
+     * @param iter An section_iter filled by get_sections()
+     * @param table Array of TableOption with information on which fields are going to be
+     * extracted and how to extract them.
+     * @param table_len The number of elements in @a table.
+     * @param data A pointer to the struct that will be used to hold the extracted data.
+     *
+     * @see get_sections()
+     */
+    int extract_options(struct section_iter *iter, const OptionsTable table[], size_t table_len,
+                        void *data);
+
+    /**
+     * Get next section name from iterator that matches the shell wildcard @a pattern.
+     *
+     * If iter.ptr is @c nullptr get_sections() will look for first occurence of @a pattern in
+     * section list and fill iter.name with a pointer to section name, iter.name_len with the
+     * size of section name and iter.ptr with an iterator to be used in consecutive calls.
+     * If iter.ptr is filled with value from previous calls, get_section() will look for consective
+     * occurences of @a pattern.
+     *
+     * @param pattern A shell wilcard pattern.
+     * @param iter A instance of struct section_iter to be filled section data that matches @a
+     * pattern.
+     *
+     * @return 0 if a section is found or -ENOENT if no section matches the pattern.
+     */
+    int get_sections(const char *pattern, struct section_iter *iter);
+
+    // Helpers
+    static int parse_bool(const char *val, size_t val_len, void *storage, size_t storage_len);
+    static int parse_str_dup(const char *val, size_t val_len, void *storage, size_t storage_len);
+    static int parse_str_buf(const char *val, size_t val_len, void *storage, size_t storage_len);
+
+#define DECLARE_PARSE_INT(_type) \
+    static int parse_##_type(const char *val, size_t val_len, void *storage, size_t storage_len)
+    DECLARE_PARSE_INT(i);
+    DECLARE_PARSE_INT(ul);
+    DECLARE_PARSE_INT(ull);
+#undef DECLARE_PARSE_INT
 
 private:
-    const char *_filename;
-    struct section *_sections = nullptr;
-    struct section *_current_section = nullptr;
+    struct conffile *_files;
+    struct section *_sections;
 
-    int _add_section(char *entry, int line);
-    int _add_config(char *entry, int line);
-    void _trim(char *str);
+    int _parse_file(const char *addr, size_t len, const char *filename);
+    struct section *_find_section(const char *section_name, size_t len);
+    struct config *_find_config(struct section *s, const char *key_name, size_t key_len);
+
+    struct section *_add_section(const char *addr, size_t len, int line, const char *filename);
+    int _add_config(struct section *s, const char *entry, size_t entry_len, const char *filename,
+                    int line);
+    void _trim(const char **str, size_t *len);
+    int _extract_options_from_section(struct section *s, const OptionsTable table[],
+                                      size_t table_len, void *data);
 };
+
+/*
+ * Helper to create the parameter storage from OptionsTable, using _field  from _struct.
+ */
+#define OPTIONS_TABLE_STRUCT_FIELD(_struct, _field)        \
+    {                                                      \
+        offsetof(_struct, _field), sizeof(_struct::_field) \
+    }
