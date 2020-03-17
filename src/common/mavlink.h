d47e9c740a7e4a4c6dea8ecc324378b6f7f509e1
Lucas De Marchi <lucas.de.marchi@gmail.com>
2020-03-17 00:51:02 -0700

    build: replace explicit gcc check by warning check
    Check if the compiler has that warning instead of checking by the gcc
    version. This allows clang to ignore those warnings as it understands
    the same pragma.

--- src/common/mavlink.h
+++ src/common/mavlink.h
@@ -13,7 +13,7 @@
  * mips, arm, etc that doesn't silently handle unaligned access,
  * you may be interested checking case by case in mavlink/pymavlink.
  */
-#if defined(__GNUC__) && __GNUC__ >= 9
+#if defined(HAVE_WADDRESS_OF_PACKED_MEMBER) && HAVE_WADDRESS_OF_PACKED_MEMBER
 #pragma GCC diagnostic ignored "-Waddress-of-packed-member"
 #endif
 #include <mavlink.h>
