3a08500e30d242fa2d98c7a7f7ec7d24a3a91ff9
Lucas De Marchi <lucas.de.marchi@gmail.com>
2021-10-26 15:53:05 -0700

    build: include src from build dir
    Otherwise we can't find the generated git_version.h header. Or when we
    find it it may be defined wrongly. Also, repository is cloned without
    tags, `git describe` mail fail. So also add some fallbacks.
    
    Fix: #228

--- tools/gen-git-version.sh
+++ tools/gen-git-version.sh
@@ -7,7 +7,15 @@ GIT_VERSION=
 
 cd $dir
 if [ -e .git ]; then
-    GIT_VERSION=\"$(git describe --tags --dirty=+ --match 'v*' 2>/dev/null)\"
+    # if repo is cloned with something like --depth=1 we may not have any tag
+    GIT_VERSION=$(git describe --tags --dirty=+ --match 'v*' 2>/dev/null) || true
+    if [ -z "$GIT_VERSION" ]; then
+	GIT_VERSION="$(git rev-parse --short HEAD)" || true
+    fi
+
+    if [ -z "$GIT_VERSION" ]; then
+	GIT_VERSION="VERSION"
+    fi
 fi
 
 if [ -z "$GIT_VERSION" ]; then
@@ -17,5 +25,5 @@ fi
 cat - <<-EOF
 #pragma once
 
-#define GIT_VERSION $GIT_VERSION
+#define GIT_VERSION "$GIT_VERSION"
 EOF
