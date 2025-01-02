d94b529a227ebf0b7412688e957d20ddf9b7d58c
Lucas De Marchi <lucas.de.marchi@gmail.com>
2025-01-02 09:44:55 -0600

    CI: Fix arch selection for newer podman and docker.io

--- tools/ci-create-rootfs.sh
+++ tools/ci-create-rootfs.sh
@@ -6,21 +6,15 @@ SCRIPT_DIR=$(dirname $(realpath ${BASH_SOURCE[0]}))
 
 ARCH=$1
 DISTRO="ubuntu:24.04"
-DISTRO_ARCH=
 
-if [ "$ARCH" == "aarch64" ]; then
-    DISTRO_ARCH="arm64v8"
-elif [ "$ARCH" == "armhf" ]; then
-    DISTRO_ARCH="arm32v7"
-else
-    DISTRO_ARCH="$ARCH"
-fi
+{
+	# setup binfmt_misc
+	# it loads the binary instead of only configuring it, so it can
+	# be used for subsequent calls, even if in another container
+	sudo podman run --rm --privileged multiarch/qemu-user-static --reset -p yes
 
-# setup binfmt_misc
-# it loads the binary instead of only configuring it, so it can
-# be used for subsequent calls, even if in another container
-sudo podman run --rm --privileged multiarch/qemu-user-static --reset -p yes
+	podman build --arch $ARCH --from docker.io/$DISTRO -t $ARCH-rootfs $SCRIPT_DIR
+	container=$(podman run --arch $ARCH -d $ARCH-rootfs)
+} >/dev/null
 
-podman build --from docker.io/$DISTRO_ARCH/$DISTRO -t $ARCH-rootfs $SCRIPT_DIR
-container=$(podman run -d $ARCH-rootfs)
 podman export $container
